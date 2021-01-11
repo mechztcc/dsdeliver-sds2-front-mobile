775f5752aaa495779fe1f12655b2933fbf2ea7a2
Mechztcc <dojathan@gmail.com>
2021-01-11 15:16:03 -0300

    Finish, no maps integration

--- src/Orders/index.tsx
+++ src/Orders/index.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { StyleSheet, ScrollView, Alert, Text } from 'react-native';
 import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
+import { useIsFocused, useNavigation } from '@react-navigation/native';
+
+
 import { fetchOrders } from '../api';
-
-
 import Header from '../Header/index';
 import OrderCard from '../OrderCard';
 import { Order } from '../types';
@@ -16,15 +17,30 @@ function Orders() {
     const[orders, setOrders] = useState<Order[]>([])
     const [isLoading, setIsLoading] = useState(false);
 
+    const navigation = useNavigation();
+    const isFocused = useIsFocused();
 
-    useEffect(() => {
+
+    const fetchData = () => {
         setIsLoading(true);
         fetchOrders()
             .then((response) => {
             setOrders(response.data)})
             .catch(err => Alert.alert('Erro ao carregar dados!'))
             .finally(() => setIsLoading(false)) 
-    }, []);
+    }
+
+    useEffect(() => {
+        if (isFocused) {
+            fetchData();
+        }
+    }, [isFocused]);
+
+    const handleOnPress = (order: Order) => {
+        navigation.navigate('OrderDetails', {
+            order
+        })
+    }
 
     return (
         <> 
@@ -35,7 +51,8 @@ function Orders() {
                 <Text> Carregando pedidos!</Text>
                 ) : (
                     orders.map(order => (
-                        <TouchableWithoutFeedback key={order.id}>
+                        <TouchableWithoutFeedback key={order.id} 
+                        onPress={() => handleOnPress(order)}>
                             <OrderCard order={order}/>
                         </TouchableWithoutFeedback>
                     ))
